10861bb4f0123909e92bc41bf07d5cf0810757f4
Harekrishna12 <93567691+Harekrishna12@users.noreply.github.com>
2026-07-17 13:20:10 +0530

    Thermostat program

--- lab02/pprb/prb1/thermostat.java
+++ lab02/pprb/prb1/thermostat.java
@@ -1,5 +1,56 @@
 package lab02.pprb.prb1;
 
-public class thermostat {
-    
+public class Thermostat {
+    private String location;
+    private int temperature;
+    private static final int MIN = 16;
+    private static final int MAX = 30;
+    private static int activeCount = 0;
+
+    public Thermostat(String location, int startTemp) {
+        this.location = location;
+        this.temperature = (startTemp >= MIN && startTemp <= MAX) ? startTemp : 22;
+        activeCount++;
+    }
+
+    public void raise() {
+        if (temperature < MAX) {
+            temperature++;
+        } else {
+            System.out.println("Already at maximum (30)");
+        }
+    }
+
+    public void lower() {
+        if (temperature > MIN) {
+            temperature--;
+        } else {
+            System.out.println("Already at minimum (16)");
+        }
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public static int getActiveCount() {
+        return activeCount;
+    }
+
+    public static void main(String[] args) {
+        Thermostat thermostat1 = new Thermostat("Living Room", 22);
+        Thermostat thermostat2 = new Thermostat("Bedroom", 25);
+
+        for (int i = 0; i < 10; i++) {
+            thermostat1.raise();
+            System.out.println("Temperature: " + thermostat1.getTemperature());
+        }
+
+        for (int i = 0; i < 20; i++) {
+            thermostat1.lower();
+            System.out.println("Temperature: " + thermostat1.getTemperature());
+        }
+
+        System.out.println("Active count: " + Thermostat.getActiveCount());
+    }
 }
